8ea5acb0c83a469b062b83366660130c00b6bea7
Sajid Ali <sajidalidatascience@gmail.com>
2026-08-03 11:35:47 +0530

    Time: 0 ms (100%), Space: 12.8 MB (40.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        for(int i=0;i<nums1.size();i++){
-            bool found=false;
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    for(int k=j+1;k<nums2.size();k++){
-                        if(nums2[k] > nums1[i]){
-                            ans.push_back(nums2[k]);
-                            found=true;
-                            break;
-                        }
-                    }
-                    break;
-                }   
+        stack<int> st;
+        unordered_map<int,int> mp;
+
+        for (int i = static_cast<int>(nums2.size()) - 1; i >= 0; --i){
+            while(!st.empty() && st.top() <=nums2[i]){
+              st.pop();
             }
-            if(!found)
-              ans.push_back(-1);
-            
+            if(st.empty())
+              mp[nums2[i]]=-1;
+            else
+              mp[nums2[i]]=st.top();
+
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(int x:nums1){
+            ans.push_back(mp[x]);
         }
         return ans;
     }
