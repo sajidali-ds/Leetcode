1bfe223de4745def697249c1d62f5337e0619100
Sajid Ali <sajidalidatascience@gmail.com>
2026-08-26 22:56:38 +0530

    Time: 0 ms (100%), Space: 8.6 MB (30.32%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>st1;
+        stack<char>st2;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='#'){
+                if(!st1.empty()){
+                    st1.pop();
+                }
+            }
+            else{
+                st1.push(s[i]);
+                
+            }
+        }
+        for(int i=0;i<t.size();i++){
+            if(t[i]=='#'){
+                if(!st2.empty()){
+                    st2.pop();
+                }
+            }
+            else{
+                st2.push(t[i]);
+            }
+            
+        }
+        while(!st1.empty() && !st2.empty()){
+
+            if(st1.top() != st2.top()){
+            return false;
+        }
+
+        st1.pop();
+        st2.pop();
+        }
+        return st1.empty() && st2.empty();
+
+
+    }
+};
