4bce3d8636dc849637d9d673be1450936d76f950
Sajid Ali <sajidalidatascience@gmail.com>
2026-09-05 03:05:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (87.17%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2)
+           return x;
+        int low=1;
+        int high=x;
+        int ans=1;
+
+        while(low<=high){
+            long long mid = low+(high - low)/2;
+            if(mid * mid <= x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+           
+        
+    }
+};
