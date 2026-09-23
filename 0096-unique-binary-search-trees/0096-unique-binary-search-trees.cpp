89989f168b798880bd97ceb483bab806773963c0
Sajid Ali <sajidalidatascience@gmail.com>
2026-09-23 18:24:54 +0530

    Time: 0 ms (100%), Space: 8 MB (43.26%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int node=2;node <=n;node++){
+            for(int root=1;root <=node;root++){
+                int left=root-1;
+                int right=node-root;
+                dp[node]+=dp[left]*dp[right];
+            }
+        }
+        return dp[n];
+    }
+};
